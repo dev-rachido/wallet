ab2087880fbcb6ef876db826ca62a35c94027ca3
dev-rachido <dev.rachido@gmail.com>
2023-10-02 22:37:30 +0200

    nieuw design

--- app/page.jsx
+++ app/page.jsx
@@ -68,14 +68,15 @@ const deleteItem = async (id) => {
 
 
   return (
-    <div className="  m-2 h-screen bg-blend-color-dodge pt-10 ">
+    <div className="  m-2  h-screen bg-blend-color-dodge opacity-90 pt-10 ">
       <EmblaCarousel />
       <h1 className="text-center text-3xl border-b-2 mb-16 w-1/4 m-auto p-5 mt-8 border-slate-500 text-cyan-800 font-semibold">
         Lijst
       </h1>
       <div className="h-100   ">
-        <div className="flex border rounded-lg w-3/4 m-auto bg-cyan-800 space-between p-5">
-          <form className=" p-2 m-auto w-3/6 mt-10  h-4/6 ">
+        <div className="md:flex extrastijl   border rounded-lg w-3/4 m-auto bg-cyan-800 space-between p-3">
+
+          <form className=" p-1 m-auto  text-cyan-950  w-full mt-10  h-4/6  ">
             <div className=" text-center flex m-auto flex-col w-2/3 p-2 space-y-8 ">
               {/* Product input */}
               <input
@@ -105,33 +106,36 @@ const deleteItem = async (id) => {
               >
                 +
               </button>
+              
             </div>
           </form>
 
-          <div className="border rounded-lg p-8  font-semibold w-4/6 bg-slate-300  ">
-            <ul className="pt-4 text-slate-600 text-left  text-xs  flex flex-col md:text-sm lg:text-lg">
+          <div className="border rounded-lg p-2  m-auto font-semibold w-4/6 bg-slate-300  ">
+            <ul className="pt-4 text-slate-600 text-left text-xs lg:gap-x-2   md:text-sm  lg:grid-cols-2 lg:grid lg:text-lg">
               {products.map((product) => (
                 <li
-                  className="uppercase min-w-fit text-xs flex m-1 w-full bg-slate-100 rounded-md p-1"
+                  className="uppercase min-w-fit text-xs flex m-1 w-full  bg-slate-100 rounded-md p-1"
                   key={product.id}
                 >
-                  <div className="w-full flex justify-between ">
+                  <div className=" lg:w-full flex justify-between m-auto ">
                     <span className="text-center"> {product.name} </span>
                     <span className="  px-1 rounded-lg mx-3 text-right ">
                       € {product.price}
                     </span>
                   </div>
+                   
                   <button 
                   onClick={() => deleteItem(product.id)}
                   className=" text-slate-600 w-fit px-2 border-l-2 border-slate-600 hover:bg-slate-200    ">
                     x
                   </button>
+
                 </li>
               ))}
             </ul>
           </div>
         </div>
-        <div className="border-4 w-9/12 mt-8  py-4 font-bold text-3xl rounded  bg-slate-200 m-auto px-10 flex flex-row justify-between text-center  ">
+        <div className="extrastijl border-4 w-9/12 mt-8  py-4 font-bold text-3xl rounded  bg-slate-200 m-auto px-10 flex flex-row justify-between text-center  ">
           <h1>Totaal</h1>
           {products.length < 1 ? (
             ""
